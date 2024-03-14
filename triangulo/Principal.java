20028ac77435dfacc0fb30dd555cb309f1c8d8b8
Marco Antonio Felix Torrez <159277886+MarcoFelix13@users.noreply.github.com>
2024-03-14 15:01:22 +0000

    Actualizacion clase triangulo

---
+++ triangulo/Principal.java
@@ -0,0 +1,12 @@
+package triangulo;
+//Proposito: dado la base y la altura
+//           de un triangulo rectanguloo, calcular
+//           el perimetro y la superficie
+
+public class Principal {    
+   public static void main(String[] args) {
+       Triangulo objTriangulo = new Triangulo();
+       objTriangulo.calcularPerimetroSupercio();
+     
+   }
+}
